ec5a21ca3cc38fc96462d53d3d7c2a6aedb6ddb9
garoofix <dgarufi@gmail.com>
2018-05-06 09:32:10 -0400

    hopefully fixed

--- site/general.js
+++ site/general.js
@@ -1,13 +1,19 @@
-var CURRENTTIME = '2018-05-01 01:20';
+var CURRENTTIME = '2018-05-01 07:20';
+
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(doTick());
 
 MITDATA.sort(function (a, b) {
     return a.date - b.date || a.time - b.time;
 });
 
 $( function() {
-    // Initialize date-pickers
-    $( "#startDate" ).datepicker({dateFormat: "mm/dd/yy"});
-    $( "#endDate" ).datepicker({dateFormat: "mm/dd/yy"});
+    LIVESTREAM.watch.forEach((value)=>{
+        $('#watchList').append(`<a href="#" onclick="removeWatch(${value})" id="watch_${value}" class="list-group-item">$${value}</a>`);
+    });
+    if(LIVESTREAM.playing){
+        $('#playbtn').html(`<i class="fas fa-pause"></i>`);
+    }
 } );
 
 // Map data to object array
@@ -28,12 +34,14 @@ $( function() {
 
 // LIVESTREAM object ***********************************
     // Default as playing
-    LIVESTREAM.playing = false;
+    LIVESTREAM.playing = true;
     // LIVESTREAM object properties
     LIVESTREAM.t = CURRENTTIME;
     LIVESTREAM.viewPeriod = 2;
     $("#currentTime").html(LIVESTREAM.t);
-    LIVESTREAM.mPerTick = 5;
+    LIVESTREAM.mPerTick = 60;
+    LIVESTREAM.priceNow = 999;
+    LIVESTREAM.watch = [20,25,30];
     LIVESTREAM.toggleOn = function(){
         if(this.playing){
             console.log('Stop');
@@ -45,23 +53,35 @@ $( function() {
         this.playing = !this.playing;
     };
 
-    // Start the timer
-    doTick();
-
 // TICK FUNCTION *****************************************
     function doTick(){
+        if(LIVESTREAM.playing){
+            addTime();
+            doDraw();
+            LIVESTREAM.watch.forEach((value)=>{
+                if(LIVESTREAM.priceNow > value){
+                    $(`#watch_${value}`).addClass('list-group-item-success');
+                }else{
+                    $(`#watch_${value}`).removeClass('list-group-item-success');
+                }
+            });
+        }
         setTimeout(function(){
-            if(LIVESTREAM.playing){
-                addTime();
-                let startDate = moment(LIVESTREAM.t).subtract(LIVESTREAM.viewPeriod, 'days').subtract(2, 'years').format('MM/DD') + '/2016';
-                let endDate = moment(LIVESTREAM.t).format('MM/DD') + '/2016';
-                let mit = getUsageFromRange(startDate, endDate);
-                let neiso = getDataFromRange(startDate, endDate);
-                drawChart(mit, neiso);
-                $("#currentTime").html(LIVESTREAM.t);
-            }
             doTick();
-        }, 3000);
+        }, 1000);
+    }
+
+    function doDraw(){
+        // let startDate = moment(LIVESTREAM.t).subtract(LIVESTREAM.viewPeriod, 'days').subtract(2, 'years').format('MM/DD') + '/2016';
+        // let endDate = moment(LIVESTREAM.t).format('MM/DD') + '/2016';
+        let startT = moment(LIVESTREAM.t).subtract(LIVESTREAM.viewPeriod, 'days').subtract(2, 'years');
+        let endT = moment(LIVESTREAM.t).subtract(2, 'years');
+        let mit = getUsageFromRange(startT, endT);
+        let neiso = getDataFromRange(startT, endT);
+        LIVESTREAM.priceNow = parseFloat(neiso[neiso.length-1].marginalPrice);
+        //LIVESTREAM.playing = false;
+        drawChart(mit, neiso);
+        $("#currentTime").html(LIVESTREAM.t);
     }
 
     function addTime(){
@@ -69,136 +89,132 @@ $( function() {
         $("#currentTime").html(LIVESTREAM.t);
     }
 
+    function addWatch(){
+        let f = LIVESTREAM.watch.filter(o => parseFloat(o) === parseFloat($('#BidPrice').val()));
+        if(f.length===0){
+            LIVESTREAM.watch.push(parseFloat($('#BidPrice').val()));
+        }
+        $('#watchList').html('');
+        LIVESTREAM.watch.forEach((value)=>{
+            $('#watchList').append(`<a href="#" onclick="removeWatch(${value})" id="watch_${value}" class="list-group-item">$${value}</a>`);
+        });
+    }
+
+    function removeWatch(v){
+        LIVESTREAM.watch = LIVESTREAM.watch.filter(o => o !== v);
+        if(LIVESTREAM.watch.length === 0){return $('#watchList').html(`<a href="#" class="list-group-item">None Listed</a>`);}
+        $('#watchList').html('');
+        LIVESTREAM.watch.forEach((value)=>{
+            $('#watchList').append(`<a href="#" onclick="removeWatch(${value})" id="watch_${value}" class="list-group-item">$${value}</a>`);
+        });
+    }
 // Charts **********************
 //need for Google Charts 
-google.charts.load('current', {packages: ['corechart', 'line']});
-google.charts.load('current', {packages: ['corechart', 'bar']});
+
 
 function run(){
     // Data from ne-iso
-    console.log(getDataFromRange());
+    //console.log(getDataFromRange());
     // Data from MIT's usage
-    console.log(getUsageFromRange());
+    //console.log(getUsageFromRange());
     updateBidPrice();
     updateBidAmount();
-    google.charts.setOnLoadCallback(drawISONEPRices); 
-    google.charts.setOnLoadCallback(drawProfitability);
 }
 
 // Function to get data within time range
-function getDataFromRange(startDate, endDate){
-    // Set default values
-    startDate   = startDate || $('#startDate').val();
-    endDate     = endDate || $('#endDate').val();
+function getDataFromRange(startT, endT){
     // Filter data
-    return ENERGYDAT.filter(o => o.date >= startDate && o.date <= endDate);
+    //return ENERGYDAT.filter(o => o.date >= startDate && o.date <= endDate);
+    let data = ENERGYDAT.filter(o => dateToMoment(o.date, o.hourEnding, 'energydat').isBetween(startT, endT));
+    data.sort(function(a, b){
+        var adt = dateToMoment(a.date, a.hourEnding, 'energydat');
+        var bdt = dateToMoment(b.date, b.hourEnding, 'energydat');
+        if (adt.isBefore(bdt)) {return -1;}else{return 1;}
+    });
+    return data;
 }
 
 // Function to get data within time range
-function getUsageFromRange(startDate, endDate){
-    // Set default values
-    startDate   = startDate || $('#startDate').val();
-    endDate     = endDate || $('#endDate').val();
+function getUsageFromRange(startT, endT){
     // Filter data
-    return MITDATA.filter(o => o.date >= startDate && o.date <= endDate);
+    let data = MITDATA.filter(o => dateToMoment(o.date, o.time).isBetween(startT, endT));
+    data.sort(function(a, b){
+        var adt = dateToMoment(a.date, a.time);
+        var bdt = dateToMoment(b.date, b.time);
+        if (adt.isBefore(bdt)) {return -1;}else{return 1;}
+    });
+    return data;
+    //console.log(startT, endT);
+}
+function dateToMoment(d, t, dtype){
+    if(dtype==='energydat'){
+        return moment(d + ' ' + t, "MM/DD/YYYY HH");
+    }
+    return moment(d + ' ' + t, "MM-DD-YYYY HH:mm");
 }
 
-function drawChart(mit, iso){
-    var data = [];
-    var labels = [];
-    // MIT usage
-        mit.forEach((line)=>{
-            data.push(line['usage']);
-            let dstr = line.date + ' ' + line.time;
-            let timeline = moment(dstr, "MM/DD/YYYY HH:mm");
-            labels.push(timeline);
+var usageChart = null;
+function drawChart(mit, isone){
+
+        var data = new google.visualization.DataTable();
+        data.addColumn('datetime', 'Time');
+        data.addColumn('number', 'Usage');
+        data.addColumn('number', 'Price');
+        LIVESTREAM.watch.forEach((w, i)=>{
+            data.addColumn('number', 'Watch ' + (i + 1));
         });
-        // console.log(data);
-        // console.log(labels[0]);
-        // console.log(labels[labels.length-1]);
-    // ISO NE
-    var ctx = document.getElementById('drawPricesChart_Div').getContext('2d');
-    ctx.canvas.width = 1000;
-    ctx.canvas.height = 600;
-    var cfg = {
-        type: 'bar',
-        animation: false,
-        data: {
-            labels: labels,
-            datasets: [{
-                label: 'Price / Usage Chart',
-                data: data,
-                type: 'scatter',
-                pointRadius: 0,
-                fill: false,
-                lineTension: 0,
-                borderWidth: 2,
-                backgroundColor: 'rgba(255,0,0,1)',
-                borderColor: 'rgba(255,0,0,1)'
-            }]
+    // MIT USAGE
+        let dat = [];
+        mit.forEach((line)=>{
+            let yr = line.date.substring(6,11);
+            let mo = parseInt(line.date.substring(0,2));
+            let day = parseInt(line.date.substring(3,5));
+            let hr = parseInt(line.time.substring(0,2));
+            let min = parseInt(line.time.substring(3,5));
+            let thisrow = [new Date(yr, mo-1, day, hr, min, 0, 0),line['usage']/1000,null];
+            LIVESTREAM.watch.forEach((w, i)=>{
+                thisrow.push(w);
+            });
+            dat.push(thisrow);
+        });
+        data.addRows(dat);
+    // ISO PRICE
+        var dataISO = new google.visualization.DataTable();
+        dat = [];
+        isone.forEach((line)=>{
+            let yr = line.date.substring(6,11);
+            let mo = parseInt(line.date.substring(0,2));
+            let day = parseInt(line.date.substring(3,5));
+            let hr = parseInt(line.hourEnding);
+            let min = 0;
+            let thisrow = [new Date(yr, mo-1, day, hr, min, 0, 0),null,parseFloat(line.marginalPrice)];
+            LIVESTREAM.watch.forEach((w, i)=>{
+                thisrow.push(null);
+            });
+            dat.push(thisrow);
+        });
+        data.addRows(dat);
+
+    var options = {
+        height: 550,
+        series: {
+            0: {targetAxisIndex: 0},
+            1: {targetAxisIndex: 1}
         },
-        options: {
-            scales: {
-                xAxes: [{
-                    type: 'time',
-                    distribution: 'series',
-                    ticks: {
-                        source: 'labels'
-                    }
-                }],
-                yAxes: [{
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'Closing price ($)'
-                    }
-                }]
-            }
-        }
+        vAxes: {
+            // Adds titles to each axis.
+            0: {title: 'Usage [MW]'},
+            1: {title: 'Price [$ / MWh]'}
+        },
+        'chartArea': {'width': '80%', 'height': '80%'},
+        curveType: 'function',
+        legend: { position: 'bottom' }
     };
-    var chart = new Chart(ctx, cfg);
+    LIVESTREAM.watch.forEach((w, i)=>{
+        options.series[2+i] = {targetAxisIndex: 1};
+    });
+
+    if(usageChart===null){usageChart = new google.visualization.LineChart(document.getElementById('chart_div'))};
+
+    usageChart.draw(data, options);
 }
-
-//console.log(ENERGYDAT[0]);
-// function drawISONEPRices() {
-//     console.log('dafljfdlgdf');
-//     var intermediate = getDataFromRange();
-//     var PriceBidGraphableArray = [];
-//     intermediate.forEach(function(element){
-//         PriceBidGraphableArray.push([element[1], element[6], BidPrice]);
-//     });
-//
-//     PriceBidGraphableArray.unshift(['Time', 'DAM LMP', "Bid Price"]);
-//     var gdata = google.visualization.arrayToDataTable(PriceBidGraphableArray);
-//     var options = {
-//         title: 'ISO-NE Price versus Bid Price',
-//         vAxis: { title: 'LMP ($/MWh)' },
-//         hAxis: { title: 'Time (Month/Day/Year/Hour)' },
-//         seriesType: 'line',
-//         series: { 1: { type: 'line' } }
-//     }
-//     var chart = new google.visualization.ComboChart(document.getElementById('drawISONEPRicesChart_Div'));
-//     chart.draw(gdata, options);
-// }
-//
-// function drawProfitability(){
-//     var intermediateISO = getDataFromRange();
-//     var intermediateMITUse = getUsageFromRange(); 
-//     var data = new google.visualization.arrayToDataTable([
-//         ['Day','Profitability'],
-//         //the results [time, profitability] , each row indivudalt
-//     ]); 
-//     var options = {
-//         title: 'Motivation Level Throughout the Day',
-//         hAxis: {
-//           title: 'Time of Day',
-//           format: 'h:mm a',
-//                 },
-//         vAxis: {
-//             title: '',
-//         }
-//     }
-//     var chart = new google.visualization.ColumnChart(document.getElementById('drawProfitability-Div'));
-//     chart.draw(data, options);
-// }
-
-    //for graph 3, citation https://canvasjs.com/jquery-charts/resizable-chart/
